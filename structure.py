6c8a99e4b7b170aa3640877e508eee2a8addd194
Ilya <in4pashchenko@gmail.com>
2016-07-18 10:49:31 +0300

    Update structure.py

--- structure.py
+++ structure.py
@@ -27,6 +27,13 @@ def create_from_mk5(fn, fmt, t0, dt, nchan, d_t, metadata):
     
 def create_from_fits(fn, t0, dt, nchan, d_t, metadata):
     pass
+    
+class DSPIterator(object):
+    """http://stackoverflow.com/a/11690539"""
+    def __init__(self, fn):
+        pass
+    def __iter__(self):
+        yield dsp
         
 class DedispersedDynSpectra(object):
     def __init__(self):
@@ -95,11 +102,17 @@ class Pipeline(object):
             candidates_3 = search_3(dd_dsp, *args, **kwargs)
             t0 += dt
 
-
+class CFX(object):
+    pass
  
 class RAPipeline(object):
     def __init__(self, exp_code, cfx_file, dsp_params, raw_data_dir, db_file):
-        pass
+        self.exp_code = exp_code
+        self.cfx_file = cfx_file
+        self.dsp_params = dsp_params
+        self.raw_data_dir = raw_data_dir
+        self.db_file = db_file
+        self.cfx = CFX(cfx_file)
         
    @property
     def exp_params(self):
@@ -108,27 +121,10 @@ class RAPipeline(object):
         MetaData.
         """
         return self.cfx.parse_cfx(self.exp_code)
-В 
-    def dsp_generator(self, m5_file, m5_params, chunk_size):
-        pass
-        
-    for m5_file, m5_params in self.exp_params.items():
-            m5_file = os.path.join(self.raw_
-       
-t0 = 0    
-while True:         
-    dsp = create_from_mk5(fn, fmt, t0, dt, nchan, d_t, metadata)
-    dd = Dedispersion(non_coher_dd)
-    prep = PreProcessing(create_ellipses)
-    dd_dsp = dd(dsp, dm_values, threads)
-    search_1 = Search(search_shear)
-    candidates_1 = search_1(dd_dsp, *args, **kwargs)
 
-    dd_dsp = prep(dd_dsp, *pre_args, **pre_kwargs)
-
-    search_2 = Search(search_ell)
-    search_3 = Search(search_clf)
-
-    candidates_2 = search_2(dd_dsp, *args, **kwargs)
-    candidates_3 = search_3(dd_dsp, *args, **kwargs)
-    t0 += dt
+    def run(self):
+        exp_candidates = defaultdict(list)
+        for m5_file, m5_params in self.exp_params.items():
+            iterator = DSPIterator(m5_file, m5_params)
+            pipeline = Pipeline(iterator, de_dispersers, pre_processers, searchers, db_file)
+            pipeline.run()
